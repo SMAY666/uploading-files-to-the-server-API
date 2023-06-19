80a6ffc1f63afd8535bcaf211f91d1154963c811
Matvey <72608908+SMAY666@users.noreply.github.com>
2023-06-20 02:14:14 +0700

    choir: Add multer

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,12 @@ import Fastify from 'fastify';
 import {ServerConfig} from './types';
 import {sequelize} from './DataBase';
 import {apiRoutes} from './routes';
+import multer from 'fastify-multer';
 
 
 const server = Fastify();
 void server.register(apiRoutes, {prefix: 'api/v1'});
+void server.register(multer.contentParser);
 export async function start(config: ServerConfig): Promise<string> {
     await sequelize.sync({force: false, alter: true});
     return new Promise((resolve, reject) => {
