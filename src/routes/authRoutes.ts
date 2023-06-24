73d6655b7a3e2cb26b1edbfa6fbfcc40c53a0f28
Matvey <72608908+SMAY666@users.noreply.github.com>
2023-06-24 16:06:57 +0700

    feet: authRotes schema

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -7,12 +7,50 @@ import {authController} from '../controllers';
 export const authRoutes: FastifyPluginCallback = (instance, opts, done) => {
     instance.post<SignUpRequest>(
         '/sign-up',
-        {},
+        {
+            schema: {
+                body: {
+                    type: 'object',
+                    properties: {
+                        email: {
+                            type: 'string',
+                            minLength: 1,
+                        //     TODO: Вставить регулярное выражение
+                        },
+                        password: {
+                            type: 'string',
+                            minLength: 6,
+                        },
+                        confirmPassword: {
+                            type: 'string',
+                            minLength: 1,
+                        },
+                    },
+                },
+            },
+        },
         authController.signUp,
     );
+
     instance.post<SignInRequest>(
         '/sign-in',
-        {},
+        {
+            schema: {
+                body: {
+                    type: 'object',
+                    properties: {
+                        email: {
+                            type: 'string',
+                            minLength: 1,
+                        },
+                        password: {
+                            type: 'string',
+                            minLength: 6,
+                        },
+                    },
+                },
+            },
+        },
         authController.signIn,
     );
     done();
